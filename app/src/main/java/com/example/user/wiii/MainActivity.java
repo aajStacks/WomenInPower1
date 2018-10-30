262adfa046bce7eef1e688acabc15d5858672152
ottoporami <jobayarsbni@gmail.com>
2018-10-30 19:16:56 -0400

    Init Commit

---
+++ app/src/main/java/com/example/user/wiii/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.user.wiii;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //private static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+    }
+
+        // Example of a call to a native method
+        public void openActivity2() {
+            Intent intent = new Intent(this, Main2Activity.class);
+            startActivity(intent);
+
+        }
+
+
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
